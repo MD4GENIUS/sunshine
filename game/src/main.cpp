13f48a19a179d79703d421e2b3ad556579fad6be
Mohamed Meghji <101439085@georgebrown.ca>
2023-05-19 22:25:53 -0400

    Lab_2 Step 6 commit

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,4 +1,6 @@
+#include "raylib.h"
 #include "rlImGui.h"
+
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
@@ -6,28 +8,44 @@ int main(void)
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
     SetTargetFPS(120);
-    rlImGuiSetup(true); //To get ImGUI ready
+    rlImGuiSetup(true);
+
+    Vector2 position = { 0, 0 };
+    Vector2 velocity = { 0, 0 };
+    float dt = GetFrameTime();
 
     bool useGUI = false;
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawFPS(10, 10);
-        EndDrawing();
-    }
+        if (IsKeyPressed(KEY_GRAVE))
+            useGUI = !useGUI;
 
-    if (IsKeyPressed(KEY_GRAVE)) useGUI = !useGUI;
-    if (useGUI)
-    {
-        rlImGuiBegin(); //to make any ImGUI window appear, start here.
-        //then call the relevant functions for each element before calling rlImGuiEnd()
+        if (useGUI)
+        {
+            rlImGuiBegin();
 
+            ImGui::SliderFloat("Velocity X", &velocity.x, -10.0f, 10.0f);
+            ImGui::SliderFloat("Velocity Y", &velocity.y, -10.0f, 10.0f);
 
+            rlImGuiEnd();
+        }
+        else
+        {
+            // Update position based on velocity
+            position.x += velocity.x * dt;
+            position.y += velocity.y * dt;
 
-        rlImGuiEnd(); //to make any ImGUI window appear, end with this
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawFPS(10, 10);
+            DrawText(TextFormat("Position: (%.2f, %.2f)", position.x, position.y), 10, 30, 20, BLACK);
+            EndDrawing();
+        }
     }
 
     CloseWindow();
     return 0;
 }
+
+
+
